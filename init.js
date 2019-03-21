df52e0868897a36488d8352b480c2d344aaf9a4b
Jason O'Neill <CreativeTechGuy@users.noreply.github.com>
2019-03-20 21:17:41 -0700

    Added init

---
+++ init.js
@@ -0,0 +1,16 @@
+/* globals data, draw */
+
+function createPlayer (playerNumber) {
+  var img = document.createElement('img');
+  img.className = 'sprites';
+  img.id = 'player' + playerNumber;
+  var player = data.player[playerNumber];
+  img.alt = player.alt;
+  img.src = player.src;
+  document.body.appendChild(img);
+  draw();
+}
+
+for (var i = 0; i < data.player.length; i++) {
+  createPlayer(i);
+}
