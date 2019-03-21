b4eea4ec4c924c774187025edd9732d47fb8f85d
Jason O'Neill <CreativeTechGuy@users.noreply.github.com>
2019-03-20 21:27:47 -0700

    Swapped x and y

--- grid.js
+++ grid.js
@@ -1,13 +1,9 @@
-var x;
-for (x = 0; x < 8; x++) { 
-
-    for(var y = 0; y < 8; y++){
-     var div =  document.createElement("div");
-     div.id = "cell-" + x + "-" + y;
-     div.className = "background-tile";
-     var container = document.getElementById("container");
+for (var y = 0; y < 8; y++) {
+  for (var x = 0; x < 8; x++) {
+    var div = document.createElement('div');
+    div.id = 'cell-' + x + '-' + y;
+    div.className = 'background-tile';
+    var container = document.getElementById('container');
     container.appendChild(div);
-
-    }
-  
+  }
 }
