fee931246e48ee86331d2adf31f2c1f8fd47fa62
abheysingh96 <abheysingh96@csu.fullerton.edu>
2019-03-20 20:58:03 -0700

    Add files via upload

--- grid.js
+++ grid.js
@@ -4,6 +4,7 @@ for (x = 0; x < 8; x++) {
     for(var y = 0; y < 8; y++){
      var div =  document.createElement("div");
      div.id = "cell-" + x + "-" + y;
+     div.className = "background-tile";
      var container = document.getElementById("container");
     container.appendChild(div);
 
