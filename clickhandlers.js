6f0ac0e4c958ea9835dd008b2f1d19339cb3fe55
Jason O'Neill <CreativeTechGuy@users.noreply.github.com>
2019-03-20 21:03:44 -0700

    Fixed bugs/typos

--- clickhandlers.js
+++ clickhandlers.js
@@ -1,69 +1,58 @@
+/* globals data, handleWrap, draw */
 
+var playerOneDown = document.getElementById('p1Down');
+var playerTwoDown = document.getElementById('p2Down');
+var playerOneUp = document.getElementById('p1Up');
+var playerTwoUp = document.getElementById('p2Up');
+var playerOneLeft = document.getElementById('p1Left');
+var playerTwoLeft = document.getElementById('p2Left');
+var playerOneRight = document.getElementById('p1Right');
+var playerTwoRight = document.getElementById('p2Right');
 
-var playerOneDown = document.getElementById("p1Down");
-var playerTwoDown = document.getElementById("p2Down");
-var playerOneUp = document.getElementById("p1UP");
-var playerTwoUp = document.getElementById("p2UP");
-var playerOneLeft = document.getElementById("p1Left");
-var playerTwoLeft = document.getElementById("p2Left");
-var playerOneRight = document.getElementById("p1Right");
-var playerOneRight = document.getElementById("p2Right");
-
-playerOneDown.onclick = function ()
-{
-  data.player[0].y--;
-  handleWrap();
-  draw();
-
-}
-playerTwoDown.onlick = function ()
-{
-  //decrement row
-  data.player[1].y--;
-  handleWrap();
-  draw();
-
-}
-playerOneUP.onclick = function ()
-{
-  //increment row
+playerOneDown.onclick = function () {
   data.player[0].y++;
   handleWrap();
   draw();
-}
-playerTwoUP.onclick = function ()
-{
-  //increment row
+};
+playerTwoDown.onclick = function () {
+  // decrement row
   data.player[1].y++;
   handleWrap();
   draw();
-}
-playerOneLeft.onclick = function ()
-{
-  //decrement column
+};
+playerOneUp.onclick = function () {
+  // increment row
+  data.player[0].y--;
+  handleWrap();
+  draw();
+};
+playerTwoUp.onclick = function () {
+  // increment row
+  data.player[1].y--;
+  handleWrap();
+  draw();
+};
+playerOneLeft.onclick = function () {
+  // decrement column
   data.player[0].x--;
   handleWrap();
   draw();
-}
-playerTwoLeft.onclick = function ()
-{
-  //decrement column
+};
+playerTwoLeft.onclick = function () {
+  // decrement column
   data.player[1].x--;
   handleWrap();
   draw();
-}
-playerOneRight.onclick = function ()
-{
-  //increment column
+};
+playerOneRight.onclick = function () {
+  // increment column
   data.player[0].x++;
   handleWrap();
   draw();
-}
-playerTwoRight.onclick = function ()
-{
-  //increment column
+};
+playerTwoRight.onclick = function () {
+  // increment column
   data.player[1].x++;
   handleWrap();
   draw();
-
-}
+};
